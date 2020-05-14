c03825f5c8a9b8c3b13679cd16cfb3e280d0b2b6
Ky Kartchner <kartchnerky@gmail.com>
2020-05-14 15:43:19 -0600

    Add user table and model

--- alpha-blog/db/schema.rb
+++ alpha-blog/db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_09_050429) do
+ActiveRecord::Schema.define(version: 2020_05_14_213920) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
