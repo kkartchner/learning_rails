b173d211ec4f7240e07fdfa663e0b4890a6ce4ad
Ky Kartchner <kartchnerky@gmail.com>
2020-05-15 09:23:09 -0600

    Add styling to a lot of pages. Add user show page.

--- alpha-blog/app/controllers/users_controller.rb
+++ alpha-blog/app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def edit
     @user = User.find(params[:id])
   end
