a179d3371efcee12465b20ae3ad161cc545f4ad7
Ky Kartchner <kartchnerky@gmail.com>
2020-05-15 22:12:41 -0600

    Added session create and session destroy to session controller. Updated some of the views to match tutorial views

--- alpha-blog/app/controllers/sessions_controller.rb
+++ alpha-blog/app/controllers/sessions_controller.rb
@@ -3,9 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    byebug
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Welcome, #{user.username}! You have successfully logged in."
+      redirect_to user
+    else
+      flash.now[:alert] = "Email and password combination incorrect"
+      render 'new'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to root_path
   end
 end
