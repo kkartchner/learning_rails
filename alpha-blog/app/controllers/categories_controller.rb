a599a48103ebe7215380525e63934be38839643a
kkartchner <kartchnerky@gmail.com>
2020-06-01 21:37:20 -0600

    Add integration test for testing if invalid categories are correctly rejected Add categories index page and tests Make tests so they require an admin to be logged in

--- alpha-blog/app/controllers/categories_controller.rb
+++ alpha-blog/app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
 
   def new
      @category = Category.new
@@ -16,6 +17,7 @@ class CategoriesController < ApplicationController
   end
 
   def index
+    @categories = Category.all
   end
 
   def show
@@ -26,5 +28,12 @@ class CategoriesController < ApplicationController
 
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = "Only admins can perform that action"
+      redirect_to categories_path
+    end
   end
 end
