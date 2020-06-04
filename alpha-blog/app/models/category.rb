c5fae86cbac62cfb1e372e48b78c3503004b4141
kkartchner <kartchnerky@gmail.com>
2020-06-04 07:53:57 -0600

    Add categories dropdown to navigation bar. Restrict dropdown items so that create new category only shows up for administrators. Add article_category table and many-to-many association Add missing styling to article views to get caught up to Udemy videos.

--- alpha-blog/app/models/category.rb
+++ alpha-blog/app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
+  has_many :article_categorys
+  has_many :articles, through: :article_categorys
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }
   validates_uniqueness_of :name
 end
