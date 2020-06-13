1c694cb1e6877cdcb596e4cf1909f16c6d840633
kkartchner <kartchnerky@gmail.com>
2020-06-13 13:41:42 -0600

    Add category partial to views/categories folder Add category pill badges to article partial and article show view Update category show page to include display of each article in the category. Add category edit and update actions to categories controller Add edit category view Add shared form partial for new and edit view templates

--- alpha-blog/app/controllers/categories_controller.rb
+++ alpha-blog/app/controllers/categories_controller.rb
@@ -13,7 +13,20 @@ class CategoriesController < ApplicationController
     else
       render 'new'
     end
+  end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category name updated successfully!"
+      redirect_to @category
+    else
+      render 'edit'
+    end
   end
 
   def index
@@ -22,6 +35,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @articles = @category.articles.all
   end
 
   private 
