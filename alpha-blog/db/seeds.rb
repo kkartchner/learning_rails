4399c0bba441aed4dc3befb672bc62f8499abdd0
Ky Kartchner <kartchnerky@gmail.com>
2020-06-18 12:52:18 -0600

    Re-Add simpleBlog. Update seed data for alpha_blog

--- alpha-blog/db/seeds.rb
+++ alpha-blog/db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: "admin@alphablog.com", username: "admin", password: "admin123", admin: true)
+
+for i in 1..3 do
+  User.create(email: "user#{i}@alphablog.com", username: "user#{i}", password: "password!#{i}")
+end
+
+%w(News Technology Sports Medicine).each do |name|
+  Category.create(name: name)
+end
+
+User.all.each do |user| 
+  random_end = 1 + rand(4)
+  for i in 1..random_end
+    article = user.articles.build(title: "Article #{i}", description: "This is a great article. Article #{i} will have you greatly intrigued.")
+    random_categories = Category.all.sample(2)
+    random_categories.each do |category|
+      article.article_categories.build(category_id: category.id)
+    end
+    article.save
+  end
+end
